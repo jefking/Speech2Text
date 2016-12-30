d860cf921bbb592e3854ed34851391f67086a80f
Jef King <jefkin@microsoft.com>
2016-12-29 18:46:31 -0800

    key gathering

--- Conversion/Program.cs
+++ Conversion/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Conversion
+﻿namespace Conversion
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter your subscription key: ");
+            var key = Console.ReadLine();
+            Console.WriteLine("Key provided is: {0}", key);
         }
     }
 }
